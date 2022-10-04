39d9159a9680d3e8b7344cb2097f426685600ffa
AYOUB-EL <52068209+bbouya@users.noreply.github.com>
2022-10-04 11:05:09 +0100

    Update test_flaskr.py

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -15,8 +15,8 @@ class TriviaTestCase(unittest.TestCase):
         """Define test variables and initialize app."""
         self.app = create_app()
         self.client = self.app.test_client
-        self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        self.database_name = "trivia"
+        self.database_path = "postgres:1234//{}/{}".format('localhost:5432', self.database_name)
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
@@ -83,7 +83,7 @@ class TriviaTestCase(unittest.TestCase):
     def test_create_question(self):
         new_Question = {
             'question': 'what is your Country?',
-            'answer': 'Tunisia',
+            'answer': 'Marocco',
             'difficulty': 2,
             'category': 1
         }
@@ -106,7 +106,7 @@ class TriviaTestCase(unittest.TestCase):
 
     def test_search_not_found(self):
         search = {
-            'searchTerm': 'blah blah blah',
+            'searchTerm': 'oblah ay',
         }
         res = self.client().post('/search', json=search)
         data = json.loads(res.data)
@@ -126,9 +126,9 @@ class TriviaTestCase(unittest.TestCase):
         self.assertEqual(data['current_category'], 'Science')
 
     def test_questions_per_category_not_found(self):
-        res = self.client().get('/categories/100/questions')
-        data = json.loads(res.data)
-        self.assertEqual(res.status_code, 404)
+        response = self.client().get('/categories/100/questions')
+        data = json.loads(response.data)
+        self.assertEqual(response.status_code, 404)
         self.assertEqual(data['success'], False)
 
 # ---------------------------------------#
